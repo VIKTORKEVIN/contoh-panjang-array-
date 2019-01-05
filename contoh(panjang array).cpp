a384413a387e6199ffd24eb71e59b2d97e9fd96d
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 22:00:25 +0700

    Add files via upload

---
+++ contoh(panjang array).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void cetak_mundur (char S[])
+{
+    int i,n;
+    for(n=0;n<S[n];n++);
+        printf("panjang array = %d\n",n);
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%C",S[i]);
+    }
+}
+int main()
+{
+    char str[50]= "anton";
+    cetak_mundur (str);
+}
